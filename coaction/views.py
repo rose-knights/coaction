bcdeb0fce4941d12d26ff741a547522fc5558fee
Alan Grissett <alan.grissett@gmail.com>
2015-03-05 18:08:14 -0500

    Add function to view single task.  URL-/tasks/<task_hash_id> METHOD - GET

--- coaction/views.py
+++ coaction/views.py
@@ -33,3 +33,12 @@ def add_task():
     db.session.add(new_task)
     db.session.commit()
     return jsonify(new_task), 201
+
+
+@coaction.route("/tasks/<task_id>")
+def view_task(task_id):
+    """Method: GET
+       Returns all data for a single task from the database."""
+    data = request.get_json()
+    task = Task.query.filter_by(id=hasher.decode(task_id)[0]).first()
+    return jsonify(task.to_dict())

d0e3ccd7fe21e0dc00a76b2981cba6d2c1ede898
Alan Grissett <alan.grissett@gmail.com>
2015-03-05 18:17:52 -0500

    Add status code return for view single task

--- coaction/views.py
+++ coaction/views.py
@@ -41,4 +41,4 @@ def view_task(task_id):
        Returns all data for a single task from the database."""
     data = request.get_json()
     task = Task.query.filter_by(id=hasher.decode(task_id)[0]).first()
-    return jsonify(task.to_dict())
+    return jsonify(task.to_dict()), 200

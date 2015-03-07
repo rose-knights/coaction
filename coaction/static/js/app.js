1e8c4b986310272af1a28fe75b874320776e70a3
zachbradshaw <bradshaw.zachary@gmail.com>
2015-03-07 11:48:55 -0500

    user login working on testusername

--- coaction/static/js/app.js
+++ coaction/static/js/app.js
@@ -258,8 +258,8 @@ app.factory('User', function () {
   return function (spec) {
     spec = spec || {};
     return {
-      username: spec.username,
-      password: spec.password,
+      username: spec.username || '',
+      password: spec.password || '',
       name: spec.name,
       email: spec.email
     };
--- src/users/user.js
+++ src/users/user.js
@@ -2,8 +2,8 @@ app.factory('User', function () {
   return function (spec) {
     spec = spec || {};
     return {
-      username: spec.username,
-      password: spec.password,
+      username: spec.username || '',
+      password: spec.password || '',
       name: spec.name,
       email: spec.email
     };
